653de4da2b57232d5062260e06a8fa08248a4df8
Harsh Raj Gupta <B520023@iiit-bh.ac.in>
2021-09-26 17:27:05 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -5,32 +5,54 @@ const express = require("express"),
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json())
 app.use(cookieParser());
 app.set('view engine', 'ejs');
 
+var location = {
+        lat: 28.535405,
+        long: 77.258151,
+        info: "hello world"
+    }
 var numUser = 0;
-var user = [];
+var user = [
+    [],
+    [],
+    []
+];
 
-app.get("/map", function(req, res){
-	if (req.cookies["User id"] >= 0)
-	{
-		res.render("map", {user: user, id: req.cookies["User id"]})
-	}
-	else
-	{
-		numUser++;
-		res.cookie("User id", numUser);
-		res.redirect("/map");
-	}
+user[1].push(location);
+location = {
+	lat: 28.534327,
+	long: 77.260082,
+	info: "wow"
+}
+user[1].push(location);
+
+app.get("/map", function(req, res) {
+    if (req.cookies["User id"] >= 0) {
+        res.render("map", { user: user, id: req.cookies["User id"] })
+    } else {
+        numUser++;
+        res.cookie("User id", numUser);
+        user.push([]);
+        res.redirect("/map");
+    }
 })
 
-app.post("/add", function(req, res){
-	var location = {
-		lat : position.coords.latitude,
-		long : position.coords.longitude,
-		info : req.body.info
-	};
-	user[req.cookies["User id"]].push(location);
+app.post("/add", function(req, res) {
+    var loc = req.body.button;
+    console.log(loc[0]);
+
+    var location = {
+        lat: loc[0],
+        long: loc[1],
+        info: req.body.info
+    }
+    console.log(location);
+    console.log(req.cookies["User id"]);
+    user[req.cookies["User id"]].push(location);
+    res.redirect("/map");
 })
 
 var port = process.env.PORT || 2202;
